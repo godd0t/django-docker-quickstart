4d34336f2a9a5cb1a6124b174d7e77c62b0afe9f
godd0t <lirrishala@gmail.com>
2023-05-10 13:33:56 +0200

    add default storage

--- src/project_name/settings/prod.py
+++ src/project_name/settings/prod.py
@@ -1,6 +1,9 @@
 from .base import *  # noqa
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
